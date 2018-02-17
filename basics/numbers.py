297255d6f4b745bb954f6e047bd184de708896fc
tanzahmed <ahmedtufailtanzeem@gmail.com>
2018-02-17 14:20:13 +0530

    number types

---
+++ basics/numbers.py
@@ -0,0 +1,41 @@
+num = 2
+print(num)
+print(type(num))
+
+num = 2.2
+print(num)
+print(type(num))
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+print(3 % 2)
+
+# All bodmas rules apply as in regular arithemetic operations
+print((3+2) ** 4)
+
+# functions
+print(abs(-34))
+print(round(34.456))
+print(round(34.546))
+print(round(34.456, 2))
+
+# Comparision
+# == != < > <= >=
+
+# Castings
+ints = '123'
+floats = '123.45'
+
+print(type(ints))
+print(type(int(ints)))
+
+print(type(floats))
+values = float(floats)
+print(values)
+print(type(values))
+
+
